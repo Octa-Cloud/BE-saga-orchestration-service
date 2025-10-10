2b86bb8ef6cc16feba2f89bc759bb09b725a45e7
KoungQ <guu119@naver.com>
2025-10-10 14:11:55 +0900

    init

---
+++ src/main/java/com/project/saga/SagaOrchestrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.saga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaOrchestrationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagaOrchestrationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/saga/SagaOrchestrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.saga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaOrchestrationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
